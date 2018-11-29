d5083a495b20c0a30a51055369087d1e3e78c667
xizi <zackxizi@qq.com>
2018-11-30 01:23:27 +0800

    初始化项目

---
+++ src/views/index..vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="global-container">
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component({
+        components: {},
+    })
+    export default class Index extends Vue {
+    }
+</script>
+<style lang="scss">
+</style>
